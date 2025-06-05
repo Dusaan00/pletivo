8fad6c9e7b080870aeb1bf4efeecfa4fb4f48c63
Dusaan00 <dusa678@gmail.com>
2025-06-05 17:43:04 +0200

    fav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,21 +58,24 @@ export default function RootLayout({
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href={`${isProd ? "/pletivo" : ""}/apple-touch-icon.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href={`${isProd ? "/pletivo" : ""}/favicon-32x32.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href={`${isProd ? "/pletivo" : ""}/favicon-16x16.png`}
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="manifest"
+          href={`${isProd ? "/pletivo" : ""}/site.webmanifest`}
+        />
       </head>
       <body
         style={
