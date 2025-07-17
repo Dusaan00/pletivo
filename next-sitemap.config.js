a1dc951600a89c216101b036887d2f0bcfc6bc79
Dusaan00 <dusa678@gmail.com>
2025-07-17 17:44:53 +0200

    xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://pletivogrygov.cz", // ✅ your real domain
+  generateRobotsTxt: true, // ✅ also generates robots.txt
+  outDir: "./public", // Output the sitemap in the public folder
+};
