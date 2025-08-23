d2e069752ab39c27e9c34bf55182244c14ff8cf7
Dusaan00 <dusa678@gmail.com>
2025-08-23 16:20:27 +0200

    gg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,15 @@ import Over from "./Components/Overview";
 import Proc from "./Components/Proc";
 import Features from "./Components/Features";
 import About from "./Components/About";
+import Okno from "./functions/Okno";
 
 export default function Home() {
   return (
     <div>
+      <Okno
+        title="Do 28.8. 2025 máme zavřeno"
+        message="Všechny poptávky, emaily a zprávy začneme vyřizovat 29.8. 2025, děkujeme za pochopení."
+      />
       <Intro />
       <Over />
       <Proc />
