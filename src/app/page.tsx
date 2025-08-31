1833cda5b0ba5fb4f280e38206a5a99a629242ff
Dusaan00 <dusa678@gmail.com>
2025-08-31 23:09:16 +0200

    off

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,6 @@ import Okno from "./functions/Okno";
 export default function Home() {
   return (
     <div>
-      <Okno
-        title="Do 28.8. 2025 máme zavřeno"
-        message="Všechny poptávky, emaily a zprávy začneme vyřizovat 29.8. 2025, děkujeme za pochopení."
-      />
       <Intro />
       <Over />
       <Proc />
