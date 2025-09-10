3d7230342f108c84e13c08cb1e51bc667bc8c510
Dusaan00 <dusa678@gmail.com>
2025-09-10 19:28:50 +0200

    aa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Over from "./Components/Overview";
 import Proc from "./Components/Proc";
 import Features from "./Components/Features";
 import About from "./Components/About";
-import Okno from "./functions/Okno";
 
 export default function Home() {
   return (
