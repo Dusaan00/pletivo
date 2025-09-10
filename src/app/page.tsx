4ed9d69fb703cb8dc041306c82c357da9decc9f0
Dusaan00 <dusa678@gmail.com>
2025-09-10 14:27:10 +0200

    vacay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,6 @@ import Okno from "./functions/Okno";
 export default function Home() {
   return (
     <div>
-      <Okno
-        title="Od 4.9. do 9.9. 2025 máme zavřeno, děkujeme za pochopení"
-        message="Všechny přijaté poptávky začneme zpracovávat 10.9. 2025."
-      />
       <Intro />
       <Over />
       <Proc />
