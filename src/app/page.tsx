27c3ea328453164ff6c272d980aef05e0db75ada
Dusaan00 <dusa678@gmail.com>
2026-01-25 19:28:25 +0100

    landingh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Over from "./Components/Overview";
 import Proc from "./Components/Proc";
 import Features from "./Components/Features";
 import About from "./Components/About";
-import Akce from "./Components/Akce";
+// import Akce from "./Components/Akce";
 
 export default function Home() {
   return (
