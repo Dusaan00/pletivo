ee179c4076e38d2b5f5c5610393efa49cd7179a9
Dusaan00 <dusa678@gmail.com>
2026-01-27 00:49:28 +0100

    prehled

--- src/app/Components/Overview.tsx
+++ src/app/Components/Overview.tsx
@@ -5,114 +5,141 @@ import { basePath } from "../functions/Env";
 
 function Overview() {
   return (
-    <div className="over-container">
-      <h1 className="over-title">Sortiment</h1>
+    <section className="over-wrapper">
+      <div className="over-title">
+        <h1 className="over-title-h1">Jaké oplocení hledáte?</h1>
+      </div>
+
       <div className="over-grid">
-        <div className="over-item">
-          <Link href="/Pletivo">
-            <Image
-              src={`${basePath}/pletivaa/pletvo.webp`}
-              width={350}
-              height={350}
-              className="over-photo"
-              alt="Prodej pletiva"
-            />
-          </Link>
-          <h2 className="over-subtitle">Pletiva</h2>
+        <div className="over-product">
+          <div className="over-overlay"></div>
+          <div className="product-img">
+            <img src={`${basePath}/land/pletvo.webp`} alt="Pletivo PVC" />
+          </div>
+          <div className="product-details">
+            <h2>Pletiva PVC</h2>
+            <ul className="product-features">
+              <li>Poplastované pletivo</li>
+              <li>Efektivní, cenově dostupné oplocení</li>
+              <li>Dostupné výšky od 1 m do 2 m</li>
+            </ul>
+            <div className="product-btn">
+              <Link href="/Prehled">
+                <button className="product-btnbtn">Detaily produktu</button>
+              </Link>
+            </div>
+          </div>
         </div>
 
-        <div className="over-item">
-          <Link href="/Panely3D2D">
-            <Image
-              src={`${basePath}/panely-pletivo-grygov.png`}
-              width={350}
-              height={350}
-              className="over-photo"
-              alt="Prodej panelů 3D/2D"
+        <div className="over-product">
+          <div className="over-overlay"></div>
+          <div className="product-img">
+            <img
+              src={`${basePath}/land/bezinalv.webp`}
+              alt="Pletivo zinkové bezinal"
             />
-          </Link>
-          <h2 className="over-subtitle">
-            Panely 3D/2D -{" "}
-            <Link href="/AkcePanely" className="akce-red">
-              nyní v akci
-            </Link>
-          </h2>
+          </div>
+          <div className="product-details">
+            <h2>Pletiva Bezinal</h2>
+            <ul className="product-features">
+              <li>Velice odolné, ideální pro průmyslové oblasti</li>
+              <li>Efektivní, cenově dostupné oplocení</li>
+              <li>Dostupné výšky od 1 m do 2 m</li>
+            </ul>
+            <div className="product-btn">
+              <Link href="/Prehled">
+                <button className="product-btnbtn">Detaily produktu</button>
+              </Link>
+            </div>
+          </div>
         </div>
 
-        <div className="over-item">
-          <Link href="/Brany">
-            <Image
-              src={`${basePath}/gates/ggate.webp`}
-              width={350}
-              height={350}
-              className="over-photo"
-              alt="Prodej brán a branek"
-            />
-          </Link>
-          <h2 className="over-subtitle">Brány & Branky</h2>
+        <div className="over-product">
+          <div className="over-overlay"></div>
+          <div className="product-img">
+            <img src={`${basePath}/panelyintroo.png`} alt="Panely 3D" />
+          </div>
+          <div className="product-details">
+            <h2>Panely 3D/2D</h2>
+            <ul className="product-features">
+              <li>Panely PVC a zinkové</li>
+              <li>Estetické a moderní oplocení</li>
+              <li>Možnost postavit i s podhrabovými deskami</li>
+            </ul>
+            <div className="product-btn">
+              <Link href="/Prehled">
+                <button className="product-btnbtn">Detaily produktu</button>
+              </Link>
+            </div>
+          </div>
         </div>
 
-        <div className="over-item">
-          <Link href="/PodhraboveDesky">
-            <Image
-              src={`${basePath}/podhrabove-desky-pletivo-grygov.jpg`}
-              width={350}
-              height={350}
-              className="over-photo"
-              alt="Prodej podhrabových desek"
-              loading="lazy"
+        <div className="over-product">
+          <div className="over-overlay"></div>
+          <div className="product-img">
+            <img
+              src={`${basePath}/land/zeberkove.webp`}
+              alt="Žebérkové pletivo"
             />
-          </Link>
-          <h2 className="over-subtitle">Podhrabové desky</h2>
+          </div>
+          <div className="product-details">
+            <h2>Žeberkové oplocení</h2>
+            <ul className="product-features">
+              <li>
+                Efektivní řešení zabezpečení různých prostor vašeho pozemku
+              </li>
+              <li>Vyrábíme a dodáváme na objednávku</li>
+            </ul>
+            <div className="product-btn">
+              <Link href="/Prehled">
+                <button className="product-btnbtn">Detaily produktu</button>
+              </Link>
+            </div>
+          </div>
         </div>
 
-        <div className="over-item">
-          <Link href="/SloupkyPrislusenstvi">
-            <Image
-              src={`${basePath}/sloupky-pletivo-grygov.jpg`}
-              width={350}
-              height={350}
-              className="over-photo"
-              alt="Sloupky k oplocení na prodej"
-              loading="lazy"
-            />
-          </Link>
-          <h2 className="over-subtitle">Sloupky & příslušenství</h2>
+        <div className="over-product">
+          <div className="over-overlay"></div>
+          <div className="product-img">
+            <img src={`${basePath}/land/mobile.webp`} alt="Mobilní oplocení" />
+          </div>
+          <div className="product-details">
+            <h2>Mobilní oplocení</h2>
+            <ul className="product-features">
+              <li>Rychlé řešení zabezpečení a ohrazení prostor</li>
+              <li>Vhodné pro stavby, akce i soukromé účely</li>
+            </ul>
+            <div className="product-btn">
+              <Link href="/Prehled">
+                <button className="product-btnbtn">Detaily produktu</button>
+              </Link>
+            </div>
+          </div>
         </div>
 
-        <div className="over-item">
-          <Link
-            href="https://ploty-grygov.cz/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src={`${basePath}/stavby-pletivo-grygov.png`}
-              width={350}
-              height={350}
-              className="over-photo"
-              alt="Stavby plotů na klíč"
-              loading="lazy"
-            />
-          </Link>
-          <h2 className="over-subtitle">Stavby plotů na klíč</h2>
-        </div>
-
-        <div className="over-item">
-          <Link href="/MobilniOploceni">
-            <Image
-              src={`${basePath}/mobile/mobile.png`}
-              width={350}
-              height={350}
-              className="over-photo"
+        <div className="over-product">
+          <div className="over-overlay"></div>
+          <div className="product-img">
+            <img
+              src={`${basePath}/land/sloupkyland.webp`}
               alt="Mobilní oplocení"
-              loading="lazy"
             />
-          </Link>
-          <h2 className="over-subtitle">Mobilní oplocení</h2>
+          </div>
+          <div className="product-details">
+            <h2>Sloupky a příslušenství</h2>
+            <ul className="product-features">
+              <li>Vše nezbytné pro kompletní montáž plotu</li>
+              <li>Sloupky, podhrabové desky, vázací dráty, atd.</li>
+            </ul>
+            <div className="product-btn">
+              <Link href="/Prehled">
+                <button className="product-btnbtn">Detaily produktu</button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
