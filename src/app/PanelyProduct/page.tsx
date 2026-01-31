cf11c70d8c53b05bc801653c2acce71b0e3adfe0
Dusaan00 <dusa678@gmail.com>
2026-02-01 00:21:13 +0100

    paypanel

--- src/app/PanelyProduct/page.tsx
+++ src/app/PanelyProduct/page.tsx
@@ -1,4 +1,5 @@
 import BuyPanely from "../Components/BuyPanely";
+import Pay from "../Components/Pay";
 
 export const metadata = {
   title: "Pletivo Grygov - Panely 3D/2D",
@@ -28,5 +29,10 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <BuyPanely />;
+  return (
+    <>
+      <BuyPanely />
+      <Pay />
+    </>
+  );
 }
