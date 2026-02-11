f26dd223cbb92cf257476f874cbff127b4139a74
Dusaan00 <dusa678@gmail.com>
2026-02-11 16:58:52 +0100

    desky

--- src/app/Desky/Desky.tsx
+++ src/app/Desky/Desky.tsx
@@ -30,20 +30,6 @@ function Desky() {
 
         <div className="desky-item">
           <Image
-            src={`${basePath}/deskyy/plast.webp`}
-            alt="Plastové podhrabové desky"
-            width={300}
-            height={500}
-            className="desky-img"
-          />
-          <p className="desky-text">Podhrabové desky – plastové</p>
-          <Link href="/form">
-            <button className="desky-button">Odeslat poptávku</button>
-          </Link>
-        </div>
-
-        <div className="desky-item">
-          <Image
             src={`${basePath}/deskyy/drzaky.webp`}
             alt="Držáky podhrabových desek"
             width={300}
@@ -74,27 +60,6 @@ function Desky() {
               desky, což přidává plotu odolnost a ochranu navíc, zároveň dodají
               vašemu plotu i estetiku a upravený vzhled. Na usazování desek jsou
               většinou potřeba 2 lidi.
-            </p>
-          </div>
-        </div>
-
-        <div className="xplain-item reverse">
-          <div className="xplain-img">
-            <Image
-              src={`${basePath}/deskyy/plast.webp`}
-              alt="Plastové podhrabové desky"
-              width={300}
-              height={400}
-            />
-          </div>
-          <div className="xplain-text">
-            <h3 className="antra">Podhrabové desky – plastové</h3>
-            <p>
-              Plastové desky mají nižší hmotnost než betonové podhrabové desky,
-              stále však jde o desky s dlouhou životností a estetickým
-              provedením. Plastové desky díky svým vlastnostem nerezaví a
-              nedochází u nich ke korozi. Díky nižší hmotnosti není usazovaní
-              těchto desek tolik fyzicky náročné.
             </p>
           </div>
         </div>
