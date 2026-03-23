eb63d3ee603b92f8befea8d3ef85c4c22ed21d9f
Dusaan00 <dusa678@gmail.com>
2026-03-23 22:28:58 +0100

    cislo

--- src/app/Components/Pay.tsx
+++ src/app/Components/Pay.tsx
@@ -20,8 +20,8 @@ function Pay() {
               <h3 className="pay-card-title">Zavolejte nám</h3>
               <p className="pay-card-text">
                 Pro objednávku/poptávku zboží nám můžete zavolat na tel. +420
-                608 474 760 nebo +420 608 705 547, na telefonu jsme dostupní
-                během pracovních dnů od 7:00 do 16:00.
+                608 474 760, na telefonu jsme dostupní během pracovních dnů od
+                7:00 do 16:00.
               </p>
             </div>
           </li>
