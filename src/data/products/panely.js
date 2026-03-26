83d7f3b3e5e5668f60ec507bc8934d05c6d746aa
Dusaan00 <dusa678@gmail.com>
2026-03-26 01:13:05 +0100

    gg

---
+++ src/data/products/panely.js
@@ -0,0 +1,81 @@
+export const panelyProducts = [
+  {
+    id: "panel-green",
+    name: "Panel 3D Zelený, drát 4mm",
+    image: "/panely/panelgreen.webp",
+    description:
+      "Panel pvc v zelené barvě, šířka 2.50m, dostupné výšky panelů od 1m do 2m. Idealní pro oplocení rodinného domu, zahrady, i firemního areálu.",
+    price: "490,-Kč",
+    link: "/PanelyProduct",
+  },
+  {
+    id: "panel-anracit",
+    name: "Panel 3D Antracit, drát 4mm",
+    image: "/panely/paneltracit.webp",
+    description:
+      "Panel pvc v antracitové barvě, šířka 2.50m, dostupné výšky panelů od 1m do 2m. Idealní pro oplocení rodinného domu, zahrady, i firemního areálu.",
+    price: "490,- Kč",
+    link: "/PanelyProduct",
+  },
+  {
+    id: "panel-3d-zinc",
+    name: "Panel 3D zinkový",
+    image: "/pletivaa/zinc.webp",
+    description:
+      "Panel 3D zinkový v zinkové barvě, šířka 2.50m, dostupné výšky panelů od 1m do 2m. Zinek dodává ochranu navíc. Zinkové panely jsou velice odolné s dlouhou životností. Ideální pro oplocení např. v oblastech průmyslu.",
+    price: "520,-Kč",
+    link: "/PanelyZinkove",
+  },
+  {
+    id: "panel-2d-zinc",
+    name: "Panel 2D zinkový, drát 5/6mm",
+    image: "/pletivaa/dvad.png",
+    description:
+      "Panel 2D zinkový v zinkové barvě, šířka 2.50m, dostupné výšky panelů od 1m do 2m. Zinek dodává ochranu navíc. Zinkové panely jsou velice odolné s dlouhou životností. Ideální pro oplocení např. v oblastech průmyslu.",
+    price: "925,-Kč",
+    link: "/PanelyZinkove",
+  },
+  {
+    id: "sloupek-jekl-zeleny",
+    name: "Sloupek JEKL - zelený",
+    image: "/sloupky/jeklgreen.webp",
+    description:
+      "Sloupky mezi které se panely při montáži plotu dosazují. Rozměr 60x40.",
+    price: "255,-Kč",
+    link: "/SloupkyProduct",
+  },
+  {
+    id: "sloupek-jekl-antracit",
+    name: "Sloupek JEKL - antracit",
+    image: "/sloupky/jeklantracit.webp",
+    description:
+      "Sloupky mezi které se panely při montáži plotu dosazují. Rozměr 60x40.",
+    price: "255,-Kč",
+    link: "/SloupkyProduct",
+  },
+  {
+    id: "sloupek-jekl-zinc",
+    name: "Sloupek JEKL - zinkový",
+    image: "/sloupky/jeklzinc.webp",
+    description:
+      "Sloupky mezi které se panely při montáži plotu dosazují. Rozměr 60x40.",
+    price: "355,-Kč",
+    link: "/SloupkyZinkoveJekl",
+  },
+  {
+    id: "objimky-green",
+    name: "Objímky zelené",
+    image: "/sloupky/objimky3d.jpeg",
+    description: "Zelené objímky pro montáž plotu z 3D panelů.",
+    price: "55,-Kč",
+    link: "/form",
+  },
+  {
+    id: "objimky-anracit",
+    name: "Objímky antracit",
+    image: "/sloupky/objimky3da.jpg",
+    description: "Objímky v antracitové barvě pro montáž plotu z 3D panelů.",
+    price: "55,-Kč",
+    link: "/form",
+  },
+];
