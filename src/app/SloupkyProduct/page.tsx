81226909d6e4150cd8f8acfda818474b4b58930a
Dusaan00 <dusa678@gmail.com>
2026-03-26 02:58:56 +0100

    pagee

--- src/app/SloupkyProduct/page.tsx
+++ src/app/SloupkyProduct/page.tsx
@@ -2,28 +2,28 @@ import BuySloupky from "../Components/BuySloupky";
 import Pay from "../Components/Pay";
 
 export const metadata = {
-  title: "Pletivo Grygov - Panely 3D/2D",
+  title: "Pletivo Grygov - Sloupky PVC",
   description:
-    "Panely 3D/2D skladem: moderní 3D panely v zelené a antracitové barvě dodávající plotu estetický vzhled, odolné zinkové panely 3D/2D, ideální pro zabezpečení objektů.",
+    "Sloupky pvc skladem: sloupky poplastované ve velikosti 60x40 a s výškami od 2m do 2.6m, sloupky máme v zelené, antracitové, a zinkové barvě.",
   keywords:
-    "panely 3D, panely 2D, zinkové panely, plotové panely, 3D panely zelené, panely antracit",
+    "plotové sloupky, poplastované sloupky, sloupky pvc, zelené sloupky, antracitové sloupky, panely 3D, panely 2D",
   openGraph: {
-    title: "Výroba Pletiva Grygov - Panely 3D/2D",
+    title: "Výroba Pletiva Grygov - Sloupky PVC",
     description:
-      "Nabízíme 3D/2D panely v zelené, antracitové a zinkové variantě, které tvoří moderní oplocení a zároveň skvěle plní účel zabezpečení pozemku.",
+      "Sloupky pvc skladem: sloupky poplastované ve velikosti 60x40 a s výškami od 2m do 2.6m, sloupky máme v zelené, antracitové, a zinkové barvě.",
     images: [{ url: "/opengraph-image.png" }],
-    url: "https://pletivogrygov.cz/Panely3D2D",
+    url: "https://pletivogrygov.cz/SloupkyProduct",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "Výroba Pletiva Grygov - Panely 3D/2D",
+    title: "Pletivo Grygov - Sloupky PVC",
     description:
-      "Nabízíme 3D/2D panely v zelené, antracitové a zinkové variantě, které tvoří moderní oplocení a zároveň skvěle plní účel zabezpečení pozemku.",
+      "Sloupky pvc skladem: sloupky poplastované ve velikosti 60x40 a s výškami od 2m do 2.6m, sloupky máme v zelené, antracitové, a zinkové barvě.",
     images: ["/opengraph-image.png"],
   },
   alternates: {
-    canonical: "https://pletivogrygov.cz/Panely3D2D",
+    canonical: "https://pletivogrygov.cz/SloupkyProduct",
   },
   robots: "index, follow",
 };
