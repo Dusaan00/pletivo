af0ef24d420f9c6d05b47449976350acb27a86b2
Dusaan00 <dusa678@gmail.com>
2026-04-22 20:28:58 +0200

    sass

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,13 @@
-/**
- * @type {import('next').NextConfig}
- */
-
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  //output: "export",
   basePath: "",
   images: {
     unoptimized: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  turbopack: {
+    root: __dirname,
   },
 };
 
